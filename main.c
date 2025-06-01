8674e06a801bbda92a98a8dc64adc597eb1e721a
npc628 <marc.barbera@students.salle.url.edu>
2025-06-01 19:16:27 +0200

    reset

--- main.c
+++ main.c
@@ -19,230 +19,39 @@ typedef struct{
 }Movie;
 
 int addActors(Actor actors[MAX_ACTORS]) {
-    int num_actors = 0;
-    char continue_adding = 'y';
-    
-    while (continue_adding == 'y' || continue_adding == 'Y') {
-        if (num_actors < MAX_ACTORS) {
-            printf("Enter actor ID: ");
-            scanf("%d", &actors[num_actors].id);
-            
-            printf("Enter actor name: ");
-            scanf("%s", actors[num_actors].name);
-            
-            num_actors++;
-            
-            printf("Do you want to add another actor? (y/n): ");
-            scanf(" %c", &continue_adding);
-        } else {
-            printf("Maximum number of actors reached!\n");
-            continue_adding = 'n';
-        }
-    }
-    
-    return num_actors;
+    //TODO: Solicitar el id y nombre del actor y añadirlo al arreglo. Habrá que preguntarle al usuario si se desea
+    // añadir más actores al arreglo, en caso contrario volver al menú principal. Se devuelve la cantidad de actores entrados al arreglo.
 }
 
 int addMovies(Movie movies[MAX_MOVIES]) {
-    int num_movies = 0;
-    char continue_adding = 'y';
-    
-    while (continue_adding == 'y' || continue_adding == 'Y') {
-        if (num_movies < MAX_MOVIES) {
-            printf("Enter movie title: ");
-            scanf("%s", movies[num_movies].title);
-            
-            printf("Enter movie year: ");
-            scanf("%d", &movies[num_movies].year);
-            
-            printf("Enter director name: ");
-            scanf("%s", movies[num_movies].director);
-            
-            movies[num_movies].nb_actors = 0;
-            char add_actor = 'y';
-            
-            while ((add_actor == 'y' || add_actor == 'Y') && movies[num_movies].nb_actors < MAX_ACTORS) {
-                printf("Enter actor ID: ");
-                scanf("%d", &movies[num_movies].actors_ids[movies[num_movies].nb_actors]);
-                movies[num_movies].nb_actors++;
-                
-                printf("Add another actor? (y/n): ");
-                scanf(" %c", &add_actor);
-            }
-            
-            num_movies++;
-            
-            printf("Add another movie? (y/n): ");
-            scanf(" %c", &continue_adding);
-        } else {
-            printf("Maximum number of movies reached!\n");
-            continue_adding = 'n';
-        }
-    }
-    
-    return num_movies;
+    //TODO: Solicitar el título, año de lanzamiento y director de la película y añadirlo al arreglo. Tened en cuenta que
+    // también se ha de solicitar el id de los actores que participen en dicha película. Se deberá en todo momento, preguntar si se desean añadir nuevos actores y/o películas.
+    // Se devuelve la cantidad de películas entrados al arreglo.
 }
 
 int getIdByName(Actor actors[], int num_actors, char name[]){
-    int found_id = -1;
-    int i = 0;
-    
-    while (i < num_actors && found_id == -1) {
-        if (strcmp(actors[i].name, name) == 0) {
-            found_id = actors[i].id;
-        }
-        i++;
-    }
-    
-    return found_id;
+    //TODO: Dado un nombre entrado por parámetro, buscar y devolver el id del actor correspondiente al mismo.
 }
 
 void getNameById(Actor actors[], int num_actors, char name[], int id){
-    int i = 0;
-    int found = 0;
-    
-    while (i < num_actors && found == 0) {
-        if (actors[i].id == id) {
-            strcpy(name, actors[i].name);
-            found = 1;
-        }
-        i++;
-    }
-    
-    if (found == 0) {
-        strcpy(name, "Actor not found");
-    }
+    //TODO: Dado un id entrado por parámetro, buscar el nombre del actor correspondiente al mismo. La variable name, es donde se depositará el nombre del actor encontrado.
 }
 
 void showMoviesByActor(Movie movies[], int num_movies, Actor actors[], int num_actors) {
-    char actor_name[MAX_TXT];
-    int actor_id = -1;
-    int found = 0;
-    int i = 0;
-    
-    printf("Enter actor name: ");
-    scanf("%s", actor_name);
-    
-    while (i < num_actors && found == 0) {
-        if (strcmp(actors[i].name, actor_name) == 0) {
-            actor_id = actors[i].id;
-            found = 1;
-        }
-        i++;
-    }
-    
-    if (found == 1) {
-        printf("\nMovies starring %s:\n", actor_name);
-        for (i = 0; i < num_movies; i++) {
-            for (int j = 0; j < movies[i].nb_actors; j++) {
-                if (movies[i].actors_ids[j] == actor_id) {
-                    printf("- %s (%d) directed by %s\n", 
-                        movies[i].title, 
-                        movies[i].year, 
-                        movies[i].director);
-                }
-            }
-        }
-    } else {
-        printf("Actor not found!\n");
-    }
+    //TODO: Se ha de solicitar el nombre del actor y mostrar el listado de películas que son protegonizadas por el mismo.
 }
 
 void reportActorsByDirector(Movie movies[], int num_movies, Actor actors[], int num_actors) {
-    char director_name[MAX_TXT];
-    FILE *file;
-    int actor_ids[MAX_ACTORS];
-    int num_unique_actors = 0;
-    int i, j, k;
-    int is_duplicate;
-    
-    printf("Enter director name: ");
-    scanf("%s", director_name);
-    
-    file = fopen("reportABD.txt", "w");
-    if (file != NULL) {
-        for (i = 0; i < num_movies; i++) {
-            if (strcmp(movies[i].director, director_name) == 0) {
-                for (j = 0; j < movies[i].nb_actors; j++) {
-                    is_duplicate = 0;
-                    for (k = 0; k < num_unique_actors; k++) {
-                        if (actor_ids[k] == movies[i].actors_ids[j]) {
-                            is_duplicate = 1;
-                        }
-                    }
-                    if (is_duplicate == 0) {
-                        actor_ids[num_unique_actors] = movies[i].actors_ids[j];
-                        num_unique_actors++;
-                    }
-                }
-            }
-        }
-        
-        fprintf(file, "Actors who worked with director %s:\n", director_name);
-        for (i = 0; i < num_unique_actors; i++) {
-            for (j = 0; j < num_actors; j++) {
-                if (actors[j].id == actor_ids[i]) {
-                    fprintf(file, "- %s\n", actors[j].name);
-                }
-            }
-        }
-        
-        fclose(file);
-        printf("Report created successfully in reportABD.txt\n");
-    } else {
-        printf("Error creating report file!\n");
-    }
+    // TODO: Se ha de solicitar el nombre del director y crear un fichero de texto llamado "reportABD.txt" con el nombre
+    //  de los actores que participen en las películas dirigidas por dicho director
 }
 
 int deleteMovieByYear(Movie movies[], int num_movies) {
-    int year;
-    int i = 0;
-    
-    printf("Enter year to delete movies from: ");
-    scanf("%d", &year);
-    
-    while (i < num_movies) {
-        if (movies[i].year == year) {
-            for (int j = i; j < num_movies - 1; j++) {
-                movies[j] = movies[j + 1];
-            }
-            num_movies--;
-        } else {
-            i++;
-        }
-    }
-    
-    printf("Deleted all movies from year %d\n", year);
-    return num_movies;
+    //TODO: Se ha de solicitar el año de la película que se desea eliminar, encontrarlas y quitarlas del arreglo
 }
 
 void reportCurrentMovies(Movie movies[], int num_movies) {
-    FILE *file;
-    int current_year = 2024;
-    int movies_written = 0;
-    int i;
-    
-    file = fopen("reportCM.bin", "wb");
-    if (file != NULL) {
-        for (i = 0; i < num_movies; i++) {
-            if (movies[i].year == current_year) {
-                movies_written++;
-            }
-        }
-        
-        fwrite(&movies_written, sizeof(int), 1, file);
-        
-        for (i = 0; i < num_movies; i++) {
-            if (movies[i].year == current_year) {
-                fwrite(&movies[i], sizeof(Movie), 1, file);
-            }
-        }
-        
-        fclose(file);
-        printf("Report created successfully in reportCM.bin\n");
-    } else {
-        printf("Error creating report file!\n");
-    }
+    // TODO: Se ha de crear un fichero de binario llamado "reportCM.bin" con las pelñiculas que solo pertenezcan al año actual
 }
 
 int main() {
